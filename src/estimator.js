afdd1b68bcacff9489abdb1715025949504584b2
Daniel Soladoye <Soladoyedaniel@gmail.com>
2020-04-08 02:41:39 +0100

    First Challenge Solution

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,19 @@
-const covid19ImpactEstimator = (data) => data;
-
+/* eslint linebreak-style: ["error", "windows"] */
+const covid19ImpactEstimator = (data) => {
+  const inputData = data;
+  const { reportedCases } = inputData;
+  const currentlyInfected1 = reportedCases * 10;
+  const currentlyInfected2 = reportedCases * 50;
+  return {
+    data: inputData,
+    impact: {
+      currentlyInfected2,
+      infectionsByRequestedTime: currentlyInfected1 * 1024
+    },
+    severeImpact: {
+      currentlyInfected2,
+      infectionsByRequestedTime: currentlyInfected2 * 1024
+    }
+  };
+};
 export default covid19ImpactEstimator;
