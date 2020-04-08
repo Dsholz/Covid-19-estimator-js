d6e030651bbb1549c082230a4bfeeb7c43dc8e9a
Daniel Soladoye <Soladoyedaniel@gmail.com>
2020-04-08 13:19:20 +0100

    Third Challenge Trial 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,12 @@
 const covid19ImpactEstimator = (data) => {
   const {
+    region,
     periodType,
     timeToElapse,
     reportedCases,
     totalHospitalBeds
   } = data;
+  const { avgDailyIncomePopulation, avgDailyIncomeInUSD } = region;
   const currentlyInfected1 = reportedCases * 10;
   const currentlyInfected2 = reportedCases * 50;
   let multiplier;
@@ -19,8 +21,21 @@ const covid19ImpactEstimator = (data) => {
   }
   const impactInfections = currentlyInfected1 * (2 ** (Math.round(multiplier / 3)));
   const severeImpactInfections = currentlyInfected2 * (2 ** (Math.round(multiplier / 3)));
+
+  const impactICUCases = 0.05 * impactInfections;
+  const severeImpactICUCases = 0.05 * severeImpactInfections;
+
+  const impactVentilatorCases = 0.02 * impactInfections;
+  const severeImpactVentilatorCases = 0.02 * severeImpactInfections;
+
   const impactSevereCases = 0.15 * impactInfections;
   const severeImpactSevereCases = 0.15 * severeImpactInfections;
+
+  const impactEconomicLoss = (impactInfections * avgDailyIncomePopulation)
+    * avgDailyIncomeInUSD * multiplier;
+  const severeImpactEconomicLoss = (severeImpactInfections * avgDailyIncomePopulation)
+    * avgDailyIncomeInUSD * multiplier;
+
   const availableHospitalBeds = 0.35 * totalHospitalBeds;
   return {
     data,
@@ -28,13 +43,19 @@ const covid19ImpactEstimator = (data) => {
       currentlyInfected: currentlyInfected1,
       infectionsByRequestedTime: impactInfections,
       severeCasesByRequestedTime: impactSevereCases,
-      hospitalBedsByRequestedTime: availableHospitalBeds - impactSevereCases
+      hospitalBedsByRequestedTime: availableHospitalBeds - impactSevereCases,
+      casesForICUByRequestedTime: impactICUCases,
+      casesForVentilatorsByRequestedTime: impactVentilatorCases,
+      dollarsInFlight: impactEconomicLoss
     },
     severeImpact: {
       currentlyInfected: currentlyInfected2,
       infectionsByRequestedTime: severeImpactInfections,
       severeCasesByRequestedTime: severeImpactSevereCases,
-      hospitalBedsByRequestedTime: availableHospitalBeds - severeImpactSevereCases
+      hospitalBedsByRequestedTime: availableHospitalBeds - severeImpactSevereCases,
+      casesForICUByRequestedTime: severeImpactICUCases,
+      casesForVentilatorsByRequestedTime: severeImpactVentilatorCases,
+      dollarsInFlight: severeImpactEconomicLoss
     }
   };
 };
